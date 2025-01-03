66ea1472159b2a7ca5399493d3376e2e8318fc68
c0d35111y <hello@helloworld.ie>
2025-01-03 00:33:32 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,42 @@
 const CACHE_NAME = 'astroalign-cache-v1';
-const urlsToCache = ['./'];
+const urlsToCache = [
+  './',
+  './index.html', // Your HTML file
+  './icon-192.png', // App icon
+  './icon-512.png' // App icon
+];
 
+// Install the service worker and cache app files
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
       return cache.addAll(urlsToCache);
     })
   );
 });
 
+// Fetch files from the cache or the network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+// Update the service worker and remove old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
